4107d5c9f0c2935ee922db53da37eae50af750a2
Andy Zhou <azhou4@stuy.edu>
2018-01-06 17:08:54 -0500

    added simplistic display for testing

--- Sudoku.java
+++ Sudoku.java
@@ -3,6 +3,15 @@ public class Sudoku{
 	
     }
     private Box[][] data;
+    private void display(){
+	for(int i = 0;i < 9;i++){
+	    String line = "";
+	    for(int x = 0;x < 9;x++){
+		line += data[i*9+x];
+	    }
+	    System.out.println(line);
+	}
+    }
 }
 
 		    
