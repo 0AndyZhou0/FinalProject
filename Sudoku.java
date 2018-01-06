e9f5deb557e3d8a90fc72ae98dae059a3af5ac03
Andy Zhou <azhou4@stuy.edu>
2018-01-06 17:10:58 -0500

    added substitute constructor for testing

--- Sudoku.java
+++ Sudoku.java
@@ -3,11 +3,20 @@ public class Sudoku{
 	
     }
     private Box[][] data;
+
+    public Sudoku(){
+	for(int i = 0;i < 9;i++){
+	    for(int x = 0;x < 9;x++){
+		data[i][x] = 0;
+	    }
+	}
+    }
+    
     private void display(){
 	for(int i = 0;i < 9;i++){
 	    String line = "";
 	    for(int x = 0;x < 9;x++){
-		line += data[i*9+x];
+		line += data[i][x];
 	    }
 	    System.out.println(line);
 	}
